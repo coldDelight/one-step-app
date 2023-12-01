79445d7cc458dd95274ab1c94385727efdfd1735
coldDelight <hno05039@naver.com>
2023-12-01 15:04:42 +0900

    [Setting] build-logic plugin생성

---
+++ build-logic/convention/src/main/java/com/colddelight/convention/KotlinAndroid.kt
@@ -0,0 +1,35 @@
+package com.colddelight.convention
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *>,
+) {
+    commonExtension.apply {
+        compileSdk = 34
+
+        defaultConfig {
+            minSdk = 26
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_18
+            targetCompatibility = JavaVersion.VERSION_18
+            //isCoreLibraryDesugaringEnabled = true
+        }
+    }
+
+    configureKotlin()
+}
+
+private fun Project.configureKotlin() {
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_18.toString()
+        }
+    }
+}
