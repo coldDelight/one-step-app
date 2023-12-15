7834ad48acb2db774697cf1b0f7187b50cd244f1
hiitsmeso <siki7878@gmail.com>
2023-12-15 16:55:27 +0900

    [Setting] designSystem Type 에 font 추가

--- core/data/src/main/java/com/colddelight/data/util/Common.kt
+++ core/data/src/main/java/com/colddelight/data/util/Common.kt
@@ -9,3 +9,17 @@ fun getTodayDate(): String{
     val dateFormat = SimpleDateFormat("yy.MM.dd")
     return dateFormat.format(currentDate)
 }
+
+fun getDayOfWeek(
+    dayOfWeek: Int
+): String{
+    return when(dayOfWeek){
+        1-> "월요일"
+        2-> "화요일"
+        3-> "수요일"
+        4-> "목요일"
+        5-> "금요일"
+        6-> "토요일"
+        else-> "일요일"
+    }
+}
--- core/designsystem/src/main/java/com/colddelight/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/colddelight/designsystem/theme/Type.kt
@@ -1,34 +1,213 @@
 package com.colddelight.designsystem.theme
 
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.sp
+import com.colddelight.designsystem.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val notosanskr = FontFamily(
+    Font(R.font.notosanskr_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.notosanskr_semibold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.notosanskr_medium, FontWeight.Medium, FontStyle.Normal),
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+
+val hotra = FontFamily(
+    Font(R.font.horta, FontWeight.Medium, FontStyle.Normal)
+)
+
+val NotoTypography = Typography(
+    headlineLarge = TextStyle(
+        color = TextGray,
+        fontFamily = notosanskr,
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    headlineMedium = TextStyle(
+        color = TextGray,
+        fontFamily = notosanskr,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    headlineSmall = TextStyle(
+        color = TextGray,
+        fontFamily = notosanskr,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    bodyLarge = TextStyle(
+        color = TextGray,
+        fontFamily = notosanskr,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    bodyMedium = TextStyle(
+        color = TextGray,
+        fontFamily = notosanskr,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    labelMedium = TextStyle(
+        color = TextGray,
+        fontFamily = notosanskr,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        color = TextGray,
+        fontFamily = notosanskr,
+        fontWeight = FontWeight.Bold,
+        fontSize = 8.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+)
+
+val HortaTypography = Typography(
+    headlineLarge = TextStyle(
+        color = TextGray,
+        fontFamily = hotra,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontSize = 40.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    headlineMedium = TextStyle(
+        color = TextGray,
+        fontFamily = hotra,
+        fontWeight = FontWeight.Medium,
+        fontSize = 32.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    headlineSmall = TextStyle(
+        color = TextGray,
+        fontFamily = hotra,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    bodyLarge = TextStyle(
+        color = TextGray,
+        fontFamily = hotra,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    bodyMedium = TextStyle(
+        color = TextGray,
+        fontFamily = hotra,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    labelMedium = TextStyle(
+        color = TextGray,
+        fontFamily = hotra,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
+    labelSmall = TextStyle(
+        color = TextGray,
+        fontFamily = hotra,
+        fontWeight = FontWeight.Medium,
+        fontSize = 8.sp,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Bottom,
+            trim = LineHeightStyle.Trim.None
+        )
+    ),
 )
