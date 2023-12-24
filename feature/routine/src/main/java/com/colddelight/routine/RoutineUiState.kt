ca3f3e7c8c744f69aecb143dd773a163d2782396
hiitsmeso <siki7878@gmail.com>
2023-12-24 17:21:44 +0900

    [Feature] 운동 등록 Bottom Sheet 작업중, All Exercise List 가져와서 필터까지 성공

--- feature/routine/src/main/java/com/colddelight/routine/RoutineUiState.kt
+++ feature/routine/src/main/java/com/colddelight/routine/RoutineUiState.kt
@@ -1,5 +1,6 @@
 package com.colddelight.routine
 
+import com.colddelight.model.ExerciseInfo
 import com.colddelight.model.Routine
 import com.colddelight.model.RoutineDayInfo
 
@@ -17,4 +18,9 @@ sealed interface RoutineDayInfoUiState {
     data class Error(val msg: String) : RoutineDayInfoUiState
 
     data class Success(val routineDayInfo: List<RoutineDayInfo>) : RoutineDayInfoUiState
+}
+
+sealed interface ExerciseListState {
+    data object None: ExerciseListState
+    data class NotNone(val exerciseList: List<ExerciseInfo>): ExerciseListState
 }
