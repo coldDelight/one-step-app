423e1e2b64849ae034c86bdedbb1a903c7aa3c7a
coldDelight <hno05039@naver.com>
2024-01-17 15:36:34 +0900

    [Feature]  운동추가 스크롤

--- feature/exercisedetail/src/main/java/com/colddelight/exercisedetail/ExerciseDetailScreen.kt
+++ feature/exercisedetail/src/main/java/com/colddelight/exercisedetail/ExerciseDetailScreen.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.os.CountDownTimer
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.util.Log
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,6 +21,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
@@ -27,16 +31,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -63,6 +70,8 @@ import com.colddelight.exercise.ExerciseUiState
 import com.colddelight.exercise.ExerciseViewModel
 import com.colddelight.model.Exercise
 import com.colddelight.model.SetInfo
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun ExerciseDetailScreen(
@@ -130,13 +139,19 @@ private fun ExerciseDetailContent(
     updateUiState: (newState: ExerciseDetailUiState) -> Unit,
     onDoneButtonClick: () -> Unit,
 ) {
+    val lazyColumnState = rememberLazyListState()
+    val density = LocalDensity.current
+    val itemSizePx = with(density) { 100.dp.toPx() }
+    val coroutineScope = rememberCoroutineScope()
 
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp, vertical = 16.dp),
+        state = lazyColumnState
+    ) {
 
-        ) {
+
         item {
             ExerciseInfo(exercise, Modifier)
         }
@@ -197,7 +212,15 @@ private fun ExerciseDetailContent(
             ClickableText(
                 text = AnnotatedString("+ 세트 추가"),
                 style = NotoTypography.headlineSmall.copy(color = DarkGray),
-                onClick = { setAction(SetAction.AddSet) })
+                onClick = {
+                    setAction(SetAction.AddSet)
+                    coroutineScope.launch {
+                        lazyColumnState.animateScrollBy(
+                            value = itemSizePx * lazyColumnState.layoutInfo.totalItemsCount.toFloat(),
+                            animationSpec = tween(durationMillis = 2000)
+                        )
+                    }
+                })
         }
 
     }
